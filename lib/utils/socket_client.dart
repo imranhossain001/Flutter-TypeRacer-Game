e5b754c8fcfd223f7de1e2f90c45be8f89397241
imran.hossain <imran.hossain.main@gmail.com>
2023-08-16 20:01:10 +0600

    first commit

---
+++ lib/utils/socket_client.dart
@@ -0,0 +1,20 @@
+import 'package:socket_io_client/socket_io_client.dart' as IO;
+
+class SocketClient {
+  IO.Socket? socket;
+  static SocketClient? _instance;
+
+  SocketClient._internal() {
+    socket =
+        IO.io('https://imranhossain001-fantastic-doodle-q94pw4rwvv6h4j6x-3000.preview.app.github.dev/', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': false,
+    });
+    socket!.connect();
+  }
+
+  static SocketClient get instance {
+    _instance ??= SocketClient._internal();
+    return _instance!;
+  }
+}
---
+++ note.md
@@ -0,0 +1,1 @@
+fully works. no need to test.
